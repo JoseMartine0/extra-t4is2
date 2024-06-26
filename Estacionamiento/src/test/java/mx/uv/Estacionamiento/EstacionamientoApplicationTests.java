42a500cd11ea50ca3d82cf4b671fa3e0d47634c1
JoseMartine0 <zs19016416@estudiantes.uv.mx>
2024-06-26 13:23:19 -0600

    commit

---
+++ Estacionamiento/src/test/java/mx/uv/Estacionamiento/EstacionamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.Estacionamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
